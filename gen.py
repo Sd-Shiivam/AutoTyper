2dd3f52a44c608e45f5c39398ce326fbef4ea799
Shivam-Singh <sd.shivam.00@gmail.com>
2022-09-12 14:23:31 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,6 +1,38 @@
 import os
 normal_text=['q',' ' ,'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0','Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
-special_text=['[', '@', '_', '!', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '?', '/', '\\', '|', '{', '}', '~', ':', ']']
+special_text1={'[':"chr(91)", 
+              '@':"chr(64)", 
+              '_':"chr(95)", 
+              '!':"chr(33)", 
+              '#':"chr(35)", 
+              '$':"chr(36)",
+              '*':"chr(42)", 
+              '<':"chr(60)", 
+              '>':"chr(62)", 
+              '?':"chr(63)", 
+              ':':"chr(58)", 
+              ']':"chr(93)", 
+              '=':"chr(61)", 
+              "'":"chr(39)"}
+
+special_text2={ 
+              '%':"{%}",
+              '+':"{+}",
+              '"':'{""}',
+              ',':'{,}',
+              '.':'{.'+'}',
+              '^':"{^}", 
+              '&':"{&}",
+              '(':"{(}", 
+              ')':"{)}",
+              '/':"{/}", 
+              '\\':"{\\}", 
+              '|':"{|}", 
+              '{': str("{{"+"}"), 
+              '}':str("{"+"}}"), 
+              '~':"{~}", 
+}
+
 f1=open('data.txt','r')
 code=f1.read()
 listcode = code.splitlines()
@@ -16,52 +48,40 @@ for l in listcode:
     word=''''''
     for i in l: 
         if i in normal_text:
-            word += i
+            # print(i)
+            # word += i
+            body += f'master.sendkeys "{i}"'+'\n'
         else:
-            body += f'master.sendkeys "{word}"'+'\n'
-            word=''''''
-        if i in special_text:
-            if '(' == str(i) :
-                body += 'master.sendkeys "{(}"'+'\n'
-            elif ')' == str(i):
-                body += 'master.sendkeys "{)}"'+'\n'
-            elif '+' == str(i):
-                body += 'master.sendkeys "{+}"'+'\n'
-            elif ':' == str(i):
-                body += 'master.sendkeys "{:}"'+'\n'
-            elif ',' == str(i):
-                body += 'master.sendkeys "{,}"'+'\n'
-            elif '=' == str(i):
-                body += 'master.sendkeys "{=}"'+'\n'
-            elif '"'+'\n' == str(i):
-                body += '''master.sendkeys "{'}"'+'\n'''
-            elif '[' == str(i):
-                body += 'master.sendkeys "{['+'}"'+'\n'
-            elif ']' == str(i):
-                body += 'master.sendkeys "{]'+'}"'+'\n'
-            elif '*' == str(i):
-                body += 'master.sendkeys "{*}"'+'\n'
-            elif '-' == str(i):
-                body += 'master.sendkeys "{-}"'+'\n'
-            elif '\\' == str(i):
-                body += 'master.sendkeys "{\\}"'+'\n'
-            elif '/' == str(i):
-                body += 'master.sendkeys "{/}"'+'\n'
-            elif '#' == str(i):
-                body += 'master.sendkeys "{#}"'+'\n'
-            elif '_' == str(i):
-                body += 'master.sendkeys "{_'+'}"'+'\n'
-            elif '<' == str(i):
-                body += 'master.sendkeys "{<}"'+'\n'
-            elif '>' == str(i):
-                body += 'master.sendkeys "{>}"'+'\n'
+            pass
+            # print(word)
+            # body += f'master.sendkeys "{word}"'+'\n'
+        for sp in special_text1:
+            if sp == i:
+                new=special_text1[sp] 
+                body += f'master.sendkeys {new}'+'\n'
+        for sp2 in special_text2:
+            if sp2 == i:
+                new2=special_text2[sp2] 
+                body += f'master.sendkeys "{new2}"'+'\n'
+        
 
     body += "\nwscript.sleep 300\n"
     body += 'master.sendkeys "{Enter'+'}"'+'\n'
     word =''' '''
 
-m = str(head)+str(body)
-# print(m)
+
+footer='''\n
+master.sendkeys "^{w}"
+wscript.sleep 300
+master.sendkeys "{Enter'''+'''}"
+wscript.sleep 300
+master.sendkeys "Code.txt"
+master.sendkeys "{Enter'''+'''}"
+master.sendkeys "{Tab"+"}"
+master.sendkeys "{Enter'''+'''}"
+wscript.sleep 300
+'''
+m = str(head)+str(body) + str(footer)
 file = open('main.vbs', 'w')
 file.write(m)
 file.close()
